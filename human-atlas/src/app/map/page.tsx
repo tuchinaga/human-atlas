57f5893a35b810374d041efb8137a1e6931f0257
tuchinaga <takahiro.uchinaga@icloud.com>
2026-07-22 23:03:12 +0900

    Remove accidentally committed duplicate project folder and stray files

--- human-atlas/src/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getMapPlaces } from "@/db/queries";
-import { MapExplorer } from "@/components/MapExplorer";
-
-export const dynamic = "force-dynamic";
-
-export default async function MapPage() {
-  const places = await getMapPlaces();
-  return <MapExplorer places={places} />;
-}
--- human-atlas/src/app/network/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getNetworkData } from "@/db/queries";
-import { NetworkView } from "@/components/NetworkView";
-
-export const dynamic = "force-dynamic";
-
-export default async function NetworkPage() {
-  const { nodes, edges } = await getNetworkData();
-  return <NetworkView nodes={nodes} edges={edges} />;
-}
